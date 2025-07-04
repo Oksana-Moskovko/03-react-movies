a289bb097be0c211c1dd7a2c48ddfe94bb41a88c
Oksana <omoskovko98@gmail.com>
2025-07-04 15:52:56 +0300

    the basis of the project

---
+++ src/components/App/App.tsx
@@ -0,0 +1,25 @@
+import css from '../App/App.module.css';
+import SearchBar from '../SearchBar/SearchBar';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import MovieModal from '../MovieModal/MovieModal';
+
+
+export default function App() {
+  // const myKey = import.meta.env.VITE_API_KEY;
+  // const myToken = import.meta.env.VITE_TMDB_TOKEN;
+
+  return (
+    <>
+      <div className={css.app}> 
+        <SearchBar />
+        <MovieGrid />
+        <Loader />
+        <ErrorMessage />
+        <MovieModal />
+      </div>
+
+    </>
+  )
+}
